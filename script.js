73671205739827fb98fcfa80fb2145d7db96c320
heolazz <heolazz.gmail.com>
2023-09-16 21:36:10 +0700

    Commit fugsi valisdasi dan tambah pendaftar

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+// Fungsi untuk validasi form dan menambahkan pendaftar
+function validateAndSubmit() {
+    const nama = document.getElementById("nama").value;
+    const umur = parseInt(document.getElementById("umur").value);
+    const uangSaku = parseInt(document.getElementById("uangSaku").value);
+
+    // Validasi kriteria
+    if (nama.length < 10 || umur < 25 || uangSaku < 100000 || uangSaku > 1000000) {
+        alert("Mohon isi form dengan benar sesuai kriteria.");
+        return;
+    } 
+
+    // Tambahkan pendaftar ke daftar
+    pendaftar.push({ nama, umur, uangSaku });
+
+    // Reset form
+    document.getElementById("registrationForm").reset();
+
+    // Update tabel pendaftar
+    updatePendaftarTable();
+
+    // Hitung rata-rata umur dan uang saku
+    hitungRataRata();
+}
+
+
+   
