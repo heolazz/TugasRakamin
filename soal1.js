d70f23c6bc601d7c41cd655c9f6a3621a55ec21c
heolazz <heolazz.gmail.com>
2023-10-01 01:11:00 +0700

    add method rl.question(ambil input panjang dan lebar dari user)

--- soal1.js
+++ soal1.js
@@ -9,13 +9,19 @@ const rl = readline.createInterface({
 
     // Mengambil input dari user
     rl.question('Masukkan panjang sisi persegi: ', (side) => {
-
+        
     // Menghitung luas dan keliling persegi
     const squareArea = modul.calculateSquareArea(parseFloat(side));
     const squarePerimeter = modul.calculateSquarePerimeter(parseFloat(side));
     
-    //Menampilkan Hasil Perhitungan Luas dan Keliling Persegi
+    // Menampilkan Hasil Perhitungan Luas dan Keliling Persegi
     console.log(`Luas persegi dengan sisi ${side} adalah ${squareArea}`);
     console.log(`Keliling persegi dengan sisi ${side} adalah ${squarePerimeter}`);
 
+    // Mengambil input dari user
+    rl.question('Masukkan panjang persegi panjang: ', (length) => {
+    rl.question('Masukkan lebar persegi panjang: ', (width) => {
+
+    });
+  });
 });
