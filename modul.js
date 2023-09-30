8ca1b416fbe1e2d18c2290e4e6a244315616b318
heolazz <heolazz.gmail.com>
2023-09-30 22:55:14 +0700

    add function square perimeter

--- modul.js
+++ modul.js
@@ -1,7 +1,12 @@
 
-// Fungsi untuk menghitung luas persegi
-function calculateSquareArea(side) {
+  // Fungsi untuk menghitung luas persegi
+  function calculateSquareArea(side) {
     return side * side;
     }
+  
+  // Fungsi untuk menghitung keliling persegi
+  function calculateSquarePerimeter(side) {
+    return 4 * side;
+  }
   
   
