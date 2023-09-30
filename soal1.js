10eae21ade1d3175848fd3ae9ce67dc0d3497e81
heolazz <heolazz.gmail.com>
2023-10-01 00:30:01 +0700

    add import moodul

---
+++ soal1.js
@@ -0,0 +1,8 @@
+// Import modul
+const modul = require('./modul.js');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
