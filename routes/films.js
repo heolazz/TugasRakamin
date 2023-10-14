da44cd5e16fd564fa59fec6b6c37c6047ae2f4ae
heolazz <heolazz.gmail.com>
2023-10-15 02:37:37 +0700

    add Route Get film by ID

--- routes/films.js
+++ routes/films.js
@@ -12,3 +12,23 @@ router.get('/', function(req,res){
         res.send(result.rows)
     })
 })
+
+// Get Film by ID
+router.get('/:id', function(req,res){
+    const filmID = req.params.id;
+    const query = {
+        text :'SELECT * FROM film WHERE film_id = $1',
+        values : [filmID],
+    };
+    
+    pool.query(query, (err, result) => {
+        if (err){
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json(result.rows);
+        }
+    })
+})
+
+module.exports = router
