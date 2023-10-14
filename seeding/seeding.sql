83963e439aaf0d67b8827eca7fd4b7d3d009ab76
heolazz <heolazz.gmail.com>
2023-10-14 18:53:17 +0700

    add seeding data actor soal1

---
+++ seeding/seeding.sql
@@ -0,0 +1,8 @@
+-- Soal 1 Seeding 5 data baru ke tabel actor
+INSERT INTO actor (first_name, last_name)
+VALUES
+    ('Harits', 'Taqiy'),
+    ('Elbert', 'Einstein'),
+    ('Dhimas', 'Johnson'),
+    ('Dafa', 'Wirayudha'),
+    ('Alysia', 'Alfi');
