927f431d87231777213ff738a2039576d6976e66
heolazz <heolazz.gmail.com>
2023-10-15 02:36:52 +0700

    add Route Get list Film

--- routes/films.js
+++ routes/films.js
@@ -3,3 +3,12 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../queries.js')
 
+// Get List Film
+router.get('/', function(req,res){
+    pool.query('SELECT * FROM film', (err, result) => {
+        if (err){
+            throw err
+        }
+        res.send(result.rows)
+    })
+})
