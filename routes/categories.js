c40cc46c7599ce0b5d40f8f9e08dd4dad8090ba8
heolazz <heolazz.gmail.com>
2023-10-15 01:21:59 +0700

    add get list Category soal 2.3

--- routes/categories.js
+++ routes/categories.js
@@ -2,3 +2,14 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../queries.js')
+
+// Get list Category soal 2.3
+router.get('/', function(req, res){
+    pool.query('SELECT * FROM category', (err, result) =>{
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error')
+        } else {res.send(result.rows)
+        }
+    })
+})
